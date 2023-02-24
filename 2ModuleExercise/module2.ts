ecf834d540052a9150df62828682069ec0193f8b
jakinyang <109392405+jakinyang@users.noreply.github.com>
2023-02-24 14:17:34 -0800

    --add Literals

--- 2ModuleExercise/module2.ts
+++ 2ModuleExercise/module2.ts
@@ -65,4 +65,20 @@ function add(x: number | string, y: number | string) {
 }
 console.log(add('one', 'two'));  //* Returns "onetwo"
 console.log(add(1, 2));          //* Returns 3
-console.log(add('one', 2));      //* Returns error
+// console.log(add('one', 2));      //* Returns error
+
+/* 
+* Literal
+*/
+
+type testResult = "pass" | "fail" | "incomplete";
+let myResult: testResult;
+myResult = "incomplete";    //* Valid
+myResult = "pass";          //* Valid
+// myResult = "failure";       //* Invalid
+
+type dice = 1 | 2 | 3 | 4 | 5 | 6;
+let diceRoll: dice;
+diceRoll = 1;    //* Valid
+diceRoll = 2;    //* Valid
+// diceRoll = 7;    //* Invalid
