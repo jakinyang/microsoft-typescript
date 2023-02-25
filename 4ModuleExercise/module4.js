13bbad557a44bcb443478917693524c7749a1e0e
jakinyang <109392405+jakinyang@users.noreply.github.com>
2023-02-25 12:00:00 -0800

    --add interfaces

---
+++ 4ModuleExercise/module4.js
@@ -0,0 +1,16 @@
+"use strict";
+function addNumbers(x, y) {
+    return x + y;
+}
+console.log(addNumbers(1, 2));
+let sum = function (input) {
+    let total = 0;
+    for (let i = 0; i < input.length; i++) {
+        if (isNaN(input[i])) {
+            continue;
+        }
+        total += Number(input[i]);
+    }
+    return total;
+};
+console.log(sum([1, 2, 3]));
---
+++ 4ModuleExercise/module4.ts
@@ -0,0 +1,18 @@
+function addNumbers(x: number, y: number): number {
+  return x + y;
+}
+
+console.log(addNumbers(1, 2));
+
+let sum = function (input: number[]): number {
+  let total: number =  0;
+  for(let i = 0; i < input.length; i++) {
+      if(isNaN(input[i])) {
+          continue;
+      }
+      total += Number(input[i]);
+  }
+  return total;
+}
+
+console.log(sum([1, 2, 3]));
