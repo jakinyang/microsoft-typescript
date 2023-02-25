653c991a67854906b626d77ee0459ae553616d93
jakinyang <109392405+jakinyang@users.noreply.github.com>
2023-02-25 12:00:00 -0800

    --add function with typed input output

--- 4ModuleExercise/module4.js
+++ 4ModuleExercise/module4.js
@@ -3,14 +3,17 @@ function addNumbers(x, y) {
     return x + y;
 }
 console.log(addNumbers(1, 2));
-let sum = function (input) {
+console.log(sum([1, 2, 3]));
+function displayAlert(message) {
+    alert(`The message is ${message}`);
+}
+function sum(input) {
     let total = 0;
-    for (let i = 0; i < input.length; i++) {
-        if (isNaN(input[i])) {
+    for (let count = 0; count < input.length; count++) {
+        if (isNaN(input[count])) {
             continue;
         }
-        total += Number(input[i]);
+        total += Number(input[count]);
     }
     return total;
-};
-console.log(sum([1, 2, 3]));
+}
--- 4ModuleExercise/module4.ts
+++ 4ModuleExercise/module4.ts
@@ -4,20 +4,19 @@ function addNumbers(x: number, y: number): number {
 
 console.log(addNumbers(1, 2));
 
-let sum = function (input: number[]): number {
-  let total: number =  0;
-  for(let i = 0; i < input.length; i++) {
-      if(isNaN(input[i])) {
-          continue;
-      }
-      total += Number(input[i]);
-  }
-  return total;
-}
-
 console.log(sum([1, 2, 3]));
 
 function displayAlert(message: string) {
   alert(`The message is ${message}`);
 }
 
+function sum(input: number[]): number {        
+  let total: number =  0;
+  for(let count = 0; count < input.length; count++) {
+      if(isNaN(input[count])){
+          continue;
+      }
+      total += Number(input[count]);
+  }
+  return total;
+}
