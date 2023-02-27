60fd6f5979215a25b617af596cf48d66660db75d
jakinyang <109392405+jakinyang@users.noreply.github.com>
2023-02-27 11:55:30 -0800

    --init typescript

---
+++ 8ModuleExercise/module8.js
@@ -0,0 +1,22 @@
+"use strict";
+var Greetings;
+(function (Greetings) {
+    function returnGreeting(greeting) {
+        console.log(`The message from namespace Greetings is ${greeting}.`);
+    }
+    Greetings.returnGreeting = returnGreeting;
+})(Greetings || (Greetings = {}));
+var GreetingsWithLength;
+(function (GreetingsWithLength) {
+    function getLength(message) {
+        return message.length;
+    }
+    function returnGreeting(greeting) {
+        let greetingLength = getLength(greeting);
+        console.log(`The message from namespace GreetingsWithLength is ${greeting} and the length of the message is ${greetingLength} characters long.`);
+    }
+    GreetingsWithLength.returnGreeting = returnGreeting;
+})(GreetingsWithLength || (GreetingsWithLength = {}));
+const message = "Hello TypeScript!";
+Greetings.returnGreeting(message);
+GreetingsWithLength.returnGreeting(message);
---
+++ 8ModuleExercise/module8.ts
@@ -0,0 +1,19 @@
+namespace Greetings {
+  export function returnGreeting(greeting: string) {
+    console.log(`The message from namespace Greetings is ${greeting}.`);
+  }
+}
+
+namespace GreetingsWithLength {
+  function getLength(message: string) {
+    return message.length;
+  }
+
+  export function returnGreeting(greeting: string) {
+    let greetingLength: number = getLength(greeting);
+    console.log(`The message from namespace GreetingsWithLength is ${greeting} and the length of the message is ${greetingLength} characters long.`);
+  }
+}
+const message = "Hello TypeScript!";
+Greetings.returnGreeting(message);
+GreetingsWithLength.returnGreeting(message);
